8df507458ecddf5ad2e43be94599896017b47534
Kavish Hukmani <khukmani@gmail.com>
2020-05-26 02:20:34 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(name='rubiks_cube_gym',
       version='0.0.1',
-      url="https://github.com/doublegremlin181/rubiks-cube-gym",
-      keywords='environment, agent, rl, openaigym, openai-gym, gym',
+      url="https://github.com/DoubleGremlin181/RubiksCubeGym/",
+      description="OpenAI Gym environments for various twisty puzzles"
+      keywords='environment, agent, rl, rubiks, cube, openai-gym, gym',
       author="Kavish Hukmani",
       author_email="khukmani@gmail.com",
       license="MIT",
