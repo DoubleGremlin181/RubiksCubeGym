1d5b300123298a3747fc10966407a88a5efc1702
Kavish Hukmani <khukmani@gmail.com>
2020-05-26 22:01:57 +0530

    Added long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(name='rubiks_cube_gym',
-      version='0.0.2',
+      version='0.0.3',
       url="https://github.com/DoubleGremlin181/RubiksCubeGym/",
       description="OpenAI Gym environments for various twisty puzzles",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       keywords='environment, agent, rl, rubiks, cube, openai-gym, gym',
       author="Kavish Hukmani",
       author_email="khukmani@gmail.com",
